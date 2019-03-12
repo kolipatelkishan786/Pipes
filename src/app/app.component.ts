4d509cb6902243b3b1a632898ef9d2f74771d72d
Kishan Patel <kolipatelkishan786@gmail.com>
2019-03-12 10:43:43 +0530

    228 Example Creating a Filter Pipe.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,7 +32,7 @@ export class AppComponent {
       started: new Date(15, 1, 17)
     }
   ];
-
+  filteredStatus = '';
   getStatusClasses(server: { instanceType: string, name: string, status: string, started: Date }) {
     return {
       'list-group-item-success': server.status === 'stable',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ShortenPipe} from './shorten.pipe';
+import { FilterPipe } from './filter.pipe';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShortenPipe
+    ShortenPipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
